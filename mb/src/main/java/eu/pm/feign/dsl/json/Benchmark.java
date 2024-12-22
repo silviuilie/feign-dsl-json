8279859340dab6bb8609cda8d01b25ce57285ee3
silviu <silviu>
2024-12-22 14:13:36 +0100

    refactor(move): move dsl-json benchmark

--- mb/src/main/java/eu/pm/feign/dsl/json/Benchmark.java
+++ mb/src/main/java/eu/pm/feign/dsl/json/Benchmark.java
@@ -1,12 +1,10 @@
-package eu.pm;
+package eu.pm.feign.dsl.json;
 
 import com.dslplatform.json.DslJson;
 import com.dslplatform.json.JsonWriter;
 import com.dslplatform.json.runtime.Settings;
 import eu.pm.serdes.TestPayload;
 import eu.pm.serdes.dslplatform.json.DslJsonDecoder;
-import eu.pm.serdes.dslplatform.json.DslJsonEncoder;
-import feign.Feign;
 import feign.Request;
 import feign.RequestTemplate;
 import feign.Response;
@@ -31,24 +29,14 @@ import java.util.concurrent.TimeUnit;
 @Measurement(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
 public class Benchmark {
 
-    public Feign defaultClient() {
-        return Feign.builder()
-                .decoder(new DslJsonDecoder())
-                .encoder(new DslJsonEncoder())
-                .build() ;
-    } 
-
-    TestPayload body = new TestPayload("name","vlad ilie ");
+    TestPayload body = new TestPayload("name", "vlad ilie ");
     DslJsonDecoder decoder = new DslJsonDecoder();
-
-
 
     DslJson<Object> dslJson = new DslJson<>(Settings.basicSetup());
     //writer should be reused. For per thread reuse use ThreadLocal pattern
     JsonWriter writer = dslJson.newWriter();
 
-    Response response ;
-
+    Response response;
 
     {
         dslJson.serialize(writer, TestPayload.class, body);
@@ -75,7 +63,7 @@ public class Benchmark {
 
 
     @org.openjdk.jmh.annotations.Benchmark
-    public void decodeRun()throws IOException {
+    public void decodeRun() throws IOException {
         decoder.decode(response, TestPayload.class);
     }
 }
