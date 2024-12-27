f4a134cd0b08db467f55fc267c62d573fec0622f
silviu <silviu>
2024-12-27 10:19:17 +0100

    doc(benchmark): reset iterations to default

--- mb/src/main/java/eu/pm/feign/jackson/JacksonEncoderBenchmark.java
+++ mb/src/main/java/eu/pm/feign/jackson/JacksonEncoderBenchmark.java
@@ -19,8 +19,8 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 @Fork(value = 1)
-@Warmup(iterations = 1, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
-@Measurement(iterations = 2, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+@Warmup(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+@Measurement(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
 public class JacksonEncoderBenchmark {
 
     TestPayload payload = new TestPayload("name", "no");
